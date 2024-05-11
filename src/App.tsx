4d46401607b8b6728d28b86f5f09e648792524d7
ryann254 <keddelyronjoz@gmail.com>
2024-05-11 09:51:18 +0300

    fix: Fix the disconnection issues with the server

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 import { IConversation } from './interfaces/convesation';
+import { IMessage } from './interfaces/message';
 
 interface IUserContent {
   name: string;
@@ -13,52 +14,66 @@ function App() {
     auth: {
       userId: '6635e291cdd94f2d13ca1687',
     },
+    transports: ['websocket', 'polling'],
+    withCredentials: true,
   });
 
   const chat = document.getElementById('chat') as HTMLDivElement;
-  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [isConnected, setIsConnected] = useState(false);
   const [userContent, setUserContent] = useState<IUserContent>({
     name: 'Edward.Green58',
     message: '',
   });
   // TODO: Create a UI that maps out all the conversations that a user has.
   const [conversations, setConversations] = useState<IConversation[]>([]);
-  console.log(conversations);
+
+  const onConnect = () => {
+    setIsConnected(true);
+  };
+
+  const onDisconnect = () => {
+    setIsConnected(false);
+  };
+
+  const onConversations = (conversations: IConversation[]) => {
+    setConversations(conversations);
+  };
 
   useEffect(() => {
-    const onConnect = () => {
-      setIsConnected(true);
-    };
-
-    const onDisconnect = () => {
-      setIsConnected(false);
-    };
-
     const onMessageSent = (message: any) => {
-      if (chat) {
-        const { fullDocument } = message;
-        const item = document.createElement('li');
-        item.textContent = fullDocument.text;
-        chat.appendChild(item);
-        window.scrollTo(0, document.body.scrollHeight);
-      }
-    };
-
-    const onConversations = (conversations: IConversation[]) => {
-      setConversations(conversations);
+      console.log(message);
+      // if (conversations[0] && conversations[0].messages.length) {
+      //   const { fullDocument } = message;
+      //   const textContent = fullDocument.text;
+      //   window.scrollTo(0, document.body.scrollHeight);
+      // }
     };
 
     socket.on('connect', onConnect);
     socket.on('disconnect', onDisconnect);
     socket.on('conversations', onConversations);
     socket.on('change', onMessageSent);
+    socket.on('connect_error', (err: Error) => {
+      // the reason of the error, for example "xhr poll error"
+      console.log('Error', err.message);
+    });
+    socket.on('connect', () => {
+      const transport = socket.io.engine.transport.name; // in most cases, "polling"
+      console.log('main transport', transport);
+
+      socket.io.engine.on('upgrade', () => {
+        const upgradedTransport = socket.io.engine.transport.name; // in most cases, "websocket"
+        console.log('upgraded transport', upgradedTransport);
+      });
+    });
 
     return () => {
       socket.off('connect', onConnect);
       socket.off('disconnect', onDisconnect);
+      socket.off('conversations', onConversations);
       socket.off('change', onMessageSent);
     };
-  }, [isConnected, socket]);
+  }, []);
 
   // const generatePassword = (length = 8) => {
   //   // Define the character sets
@@ -91,36 +106,52 @@ function App() {
       return;
     }
 
-    const data = {
-      user: {
-        id: '6635e291cdd94f2d13ca1687',
-        username: userContent.name,
-        email: 'Mollie_Mayert@example.org',
-        password: 'ryann@254',
-      },
-      message: {
-        sender: userContent.name,
-        recipientId: '6635e266cdd94f2d13ca167b',
-        recipientName: 'Grace_Schuster',
-        text: userContent.message,
-        sent: false,
-      },
-    };
+    try {
+      const data = {
+        user: {
+          id: '6635e291cdd94f2d13ca1687',
+          username: userContent.name,
+          email: 'Mollie_Mayert@example.org',
+          password: 'ryann@254',
+        },
+        message: {
+          sender: userContent.name,
+          recipientId: '6635e266cdd94f2d13ca167b',
+          recipientName: 'Grace_Schuster',
+          text: userContent.message,
+          sent: false,
+        },
+      };
 
-    await fetch(`${URL}/api/v1/message`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
+      const result = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/api/v1/message`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        }
+      );
+      console.log('result', result);
 
-    setUserContent({ ...userContent, message: '' });
+      setUserContent({ ...userContent, message: '' });
+    } catch (error) {
+      console.error('Error', error);
+    }
   };
 
   return (
     <>
-      <div id='chat'></div>
+      {conversations[0] && conversations[0].messages.length ? (
+        conversations[0].messages.map((message: IMessage, index: number) => (
+          <div key={index} id='chat'>
+            {message.text}
+          </div>
+        ))
+      ) : (
+        <></>
+      )}
       <div className='container mt-4 mb-2 xl:w-[70%] mx-auto px-4 xl:px-0 grid grid-cols-5 gap-4'>
         <div className='col-span-1 rounded-md h-11 border border-[#007bff]'>
           <input
