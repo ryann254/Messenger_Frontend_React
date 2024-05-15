0012a9913ca94faf0c383cd35397c43207c62663
ryann254 <keddelyronjoz@gmail.com>
2024-05-15 11:58:50 +0300

    feat: add text input ui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,7 +20,17 @@ const Home = () => {
           />
         ))}
       </div>
-      <div>text input</div>
+      <div className='flex px-6 py-4'>
+        <div className='h-14 w-14 mr-4 rounded-full border border-black/[.2] flex justify-center items-center'>
+          <i className='fa-solid fa-plus text-black/[.4] !font-normal text-3xl'></i>
+        </div>
+        <input
+          type='text'
+          className='w-[85%] px-4 py-2 border border-black/[.1] rounded-md focus:outline-none'
+          placeholder='Message...'
+        />
+        <i className='fa-solid fa-paper-plane absolute bottom-0 right-5'></i>
+      </div>
       {/* <MainSection /> */}
       {/* <ChannelInfo /> */}
     </div>
