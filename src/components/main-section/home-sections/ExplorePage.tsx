8c3f7ef6d5db18a9fbd9727fc18ee7b316169982
ryann254 <keddelyronjoz@gmail.com>
2024-06-06 18:19:37 +0300

    feat: add a sort function for different home sections

--- src/components/main-section/home-sections/ExplorePage.tsx
+++ src/components/main-section/home-sections/ExplorePage.tsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { SocketContext } from '@context/socket.ctx';
 
-const Conversation = ({
+export const Conversation = ({
   title,
   description,
   randomColorsGenerator,
--- src/components/main-section/home-sections/HobbyPage.tsx
+++ src/components/main-section/home-sections/HobbyPage.tsx
@@ -1,5 +1,62 @@
+import { SocketContext } from '@context/socket.ctx';
+import { useContext, useEffect, useState } from 'react';
+import { Conversation } from './ExplorePage';
+import { IConversation } from '@interfaces/convesation';
+
 const HobbyPage = () => {
-  return <div>HobbyPage</div>;
+  const { conversations } = useContext(SocketContext);
+  const [hobbyConversations, setHobbyConversations] = useState<IConversation[]>(
+    []
+  );
+
+  const randomColors: string[] = [
+    '#D97DE9',
+    '#DF6F0B',
+    '#0E77D9',
+    '#5FB918',
+    '#0B5FAE',
+    '#C91616',
+    '#16C9C9',
+    '#1616C9',
+    '#C9C916',
+    '#C916C9',
+    '#16C916',
+    '#C9C9C9',
+    '#161616',
+  ];
+
+  useEffect(() => {
+    if (conversations) {
+      const hobbyConversations = conversations.filter(
+        (conversation) => conversation.tag === 'hobby'
+      );
+      setHobbyConversations(hobbyConversations);
+    }
+  }, [conversations]);
+
+  const randomColorsGenerator = () => {
+    return randomColors[Math.floor(Math.random() * randomColors.length)];
+  };
+  return (
+    <>
+      {hobbyConversations.length ? (
+        <>
+          <h3 className='my-5 text-xl font-semibold'>Featured Conversations</h3>
+          {hobbyConversations.map((conversation, index) => (
+            <Conversation
+              key={index + conversation.name}
+              title={conversation.name}
+              description='Join our conversation to share and engage in this gaming conversation'
+              members={conversation.members.length}
+              randomColorsGenerator={randomColorsGenerator}
+            />
+          ))}
+        </>
+      ) : (
+        <div className='mt-5 font-bold text-lg'>No Conversations Yet!</div>
+      )}
+    </>
+  );
 };
 
 export default HobbyPage;
